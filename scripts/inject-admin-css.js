3cdcb1ac6e0affac47e2275307f4e62ebdb5c1d3
salvograsso10 <salvograsso10@gmail.com>
2026-08-06 15:15:28 +0200

    fix: cache no-cache su file non-hashati (_headers) e cache-busting v3 per sbloccare la CDN senza purge

--- scripts/inject-admin-css.js
+++ scripts/inject-admin-css.js
@@ -5,7 +5,7 @@ const files = ['dist/admin/index.html', 'dist/admin/login.html']
 for (const file of files) {
   if (!fs.existsSync(file)) continue
   let html = fs.readFileSync(file, 'utf8')
-  if (html.includes('css/admin.css')) continue
-  html = html.replace('</head>', '  <link rel="stylesheet" href="css/admin.css">\n</head>')
+  if (html.includes('css/admin.css?v=3')) continue
+  html = html.replace('</head>', '  <link rel="stylesheet" href="css/admin.css?v=3">\n</head>')
   fs.writeFileSync(file, html)
 }
